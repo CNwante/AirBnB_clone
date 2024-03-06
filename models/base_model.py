0e1526881ca504929cf6a411a66bc9f655289846
Donald <user@example.com>
2024-03-06 15:47:18 +0100

    created instance functions for base_model

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,7 @@ class BaseModel:
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = created_at
+        self.updated_at = datetime.now()
 
 
     def __str__(self):
@@ -56,5 +56,45 @@ class BaseModel:
             string format
         """
 
-        return '[{}] (<{}>) <{}>'.format(self.__name__,
+        return '[{}] ({}) {}'.format(type(self).__name__,
                 self.id, self.__dict__)
+
+    def save(self):
+        """
+        Update the updated_at attribute 
+
+        Args:
+            None
+
+        Return:
+            None
+        """
+
+        self.updated_at = datetime.now()
+        pass
+
+
+    def to_dict(self):
+        """
+        Create dictionary for attributes
+
+        Args:
+            None
+
+        Return:
+            None
+        """
+
+        updated_format = self.updated_at.isoformat()
+        created_format = self.created_at.isoformat()
+
+        new_dict = self.__dict__.copy()
+
+        new_dict['__class__'] = type(self).__name__
+        new_dict['created_at'] = created_format
+        new_dict['updated_at'] = updated_format
+
+        return new_dict
+
+        
+    
